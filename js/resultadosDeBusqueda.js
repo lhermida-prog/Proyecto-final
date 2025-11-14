a7987bd709ada913c9ae009a64e9890cee31d1a5
lhermida-prog <lhermida@udesa.edu.ar>
2025-11-14 17:36:46 -0300

    Edie el formulario usando lo que aprendimos hoy

--- js/resultadosDeBusqueda.js
+++ js/resultadosDeBusqueda.js
@@ -5,13 +5,19 @@ let article = document.querySelector(".randomp")
 let error = document.querySelector(".error")
 let URL = `https://dummyjson.com/products/search?q=${busqueda_usu}`
 let titulo = document.querySelector(".titulo-resultado")
+let formulario = document.querySelector(".busqueda")
+let inputBusqueda = document.querySelector("#busqueda")
 titulo.innerText = `Resultado de ${busqueda_usu}`
-if (busqueda_usu.length < 3){
-    alert("No puede tener menos de tres caracteres, volver a intentar")
-    article.style.display = "none"
-    error.style.display = "block"
-    document.querySelector("h1").style.display = "none"
-}
+formulario.addEventListener("submit", function(event){
+    event.preventDefault()
+
+    if(inputBusqueda.value.length < 3){
+        return alert("No puede tener menos de tres caracteres, volver a intentar")
+    }else{
+        this.submit()
+    }
+})
+
 fetch(URL)
 .then(function(res){
     return res.json()
@@ -21,7 +27,7 @@ fetch(URL)
     for(let i = 0; i < resultado.length && i < 10; i++){
     article.innerHTML += `
     <article class="imadiv estructura-busqueda">
-        <img src="${resultado[i].images}" class="imgin">
+        <img src="${resultado[i].thumbnail}" class="imgin">
         <h3>${resultado[i].title}</h3>
         <div class="cajas">
             <p>${resultado[i].category}</p>
