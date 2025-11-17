d73d581ad9a9a5dcebd628352bdb1dd0489c64de
adechazal1 <adechazal@udesa.edu.ar>
2025-11-17 00:09:19 -0300

    punto 6 compleamnete terminado, y empece  el de detalle

---
+++ js/categories.js
@@ -0,0 +1,21 @@
+let categorias = document.querySelector(".todas_categorias")
+
+fetch('https://dummyjson.com/products/category-list')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let contenido = "";
+        for (let i = 0; i < data.length; i++) {
+            contenido += `
+            
+            <li> <a class = "categoria" href="./category.html?idtag=${data[i]}">${data[i]}</li></a>`
+        }
+        categorias.innerHTML += contenido;
+
+    })
+    .catch(function (error) {
+        console.log('El error es: ' + error);
+    })
+
+
--- js/category.js
+++ js/category.js
@@ -1,5 +1,37 @@
+
 let catalogo = location.search
 let catalogoObj = new URLSearchParams(catalogo)
-let querry = catalogoObj.get("id")
-let url = `https://dummyjson.com/products/category/${query}`
+let querry = catalogoObj.get("idtag")
+
+let titulo = document.querySelector(".tituloproducto")
+let productos = document.querySelector(".prod")
+
+fetch(`https://dummyjson.com/products/category/${querry}`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        let contenido = ``
+        let producto = data.products;
+        for (let i = 0; i < producto.length; i++) {
+            contenido += `
+            <article class = "producto">
+                <h1>${producto[i].title}</h2>
+                <h3>Precio: ${producto[i].price}</h1>
+                <a href="./detalle.html?IdProducto=${producto[i].id}">Ver detalle</a>
+            </article>`
+        }
+        productos.innerHTML = contenido
+
+        if (producto.length ==- 0) {
+            titulo.innerHTML = `No hay productos de ${querry}`
+        }else{
+            titulo.innerHTML = `productos con categoria: ${querry}`
+        }
+                
+        }
+    )
+    .catch(function (error) {
+        console.log('El error es: ' + error);
+    })
 
