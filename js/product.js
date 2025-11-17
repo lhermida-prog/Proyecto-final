cef315651b0524b1ad2c88c453a40021c10ac47a
lhermida-prog <lhermida@udesa.edu.ar>
2025-11-17 19:11:33 -0300

    Seguí el java de producto

--- js/product.js
+++ js/product.js
@@ -1,1 +1,32 @@
-let url = "https://dummyjson.com/products/1"
+let url = "https://dummyjson.com/products"
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString)
+let query = queryStringObj.get("id")
+let section = document.querySelector(".padre-product")
+
+
+
+fetch(url)
+
+.then(function(res){
+    return res.json()
+})
+.then(function(data){
+   console.log(data);
+   let productos = data.productos
+   for(i=0; productos.lenth; i++){
+    section.innerHTML +=
+   `<section>
+   
+   `
+
+
+   }
+
+
+
+})
+.catch(function(err){
+    return err 
+})
+
